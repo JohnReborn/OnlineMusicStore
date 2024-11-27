45c09ede2ca748ea0eaf36e54df15e2efd4e3c5f
Vincent <cherryiskool@gmail.com>
2024-11-27 20:34:59 +0000

    fixing vinyl filter

--- js/merch.js
+++ js/merch.js
@@ -18,23 +18,3 @@ categoryFilter.addEventListener('change', function () {
         }
     });
 });
-
-const genreFilter = document.getElementById('genre-filter');
-const viynlList = document.getElementById('viynl-list');
-const viynlCards = document.querySelectorAll('.viynl-card');
-
-// Event listener for when the user changes the genre
-genreFilter.addEventListener('change', function () {
-    const selectedGenre = genreFilter.value;
-
-    // Loop through all viynl cards and show/hide based on the selected genre
-    viynlCards.forEach(function (card) {
-        const productGenre = card.getAttribute('data-genre');
-
-        if (selectedGenre === 'all' || productGenre === selectedGenre) {
-            card.style.display = 'block'; // Show the card
-        } else {
-            card.style.display = 'none'; // Hide the card
-        }
-    });
-});
