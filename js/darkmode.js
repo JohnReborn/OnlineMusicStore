bd0980405bef6c3f7e1499252559174006a32363
Vincent <cherryiskool@gmail.com>
2024-11-30 16:29:34 +0000

    streamlining dark mode as well as styling the index page

--- js/darkmode.js
+++ js/darkmode.js
@@ -1,12 +1,13 @@
 const button = document.getElementById("darkModeButton");
 const background = document.getElementById("middle");
 
+// local storage allows you to save "settings"
 if (localStorage.getItem("mode") =="dark"){
     background.classList.add("darkMode")
     button.classList.add("darkModeButtonDarkened")
 }
 
-
+// not sure if it helps but just tried to make the dark mode load immediately
 window.onload = () => {
     if (localStorage.getItem("mode") =="dark"){
         background.classList.add("darkMode")
@@ -14,6 +15,7 @@ window.onload = () => {
     }
 }
 
+// when the user clicks the button it first checks what mode it is already in and adds the opposite whilst saving the new mode
 button.addEventListener("click",() => {
 
     if (background.classList.contains('darkMode')){
